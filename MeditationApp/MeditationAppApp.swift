664d3978d980d27f6b3a4d3a0708cb8535314b4a
Timotius Leonardo Lianoto <lionato14@gmail.com>
2023-06-05 12:56:31 +0700

    Initial Commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Timotius Leonardo Lianoto on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
